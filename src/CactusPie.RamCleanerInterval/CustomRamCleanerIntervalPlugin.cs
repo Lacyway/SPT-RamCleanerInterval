4c2d2bc64aadcaf474e496606050a2c61469ea15
Lacyway <20912169+Lacyway@users.noreply.github.com>
2024-09-24 09:45:52 +0200

    3.10

--- src/CactusPie.RamCleanerInterval/CustomRamCleanerIntervalPlugin.cs
+++ src/CactusPie.RamCleanerInterval/CustomRamCleanerIntervalPlugin.cs
@@ -1,184 +1,172 @@
-﻿using System;
-using System.Linq;
-using System.Reflection;
-using System.Timers;
-using Aki.Reflection.Utils;
-using BepInEx;
+﻿using BepInEx;
 using BepInEx.Configuration;
 using JetBrains.Annotations;
+using System;
+using System.Timers;
 using UnityEngine;
 
 namespace CactusPie.RamCleanerInterval
 {
-    [BepInPlugin("com.cactuspie.ramcleanerinterval", "CactusPie.RamCleanerInterval", "1.0.0")]
-    public class CustomRamCleanerIntervalPlugin : BaseUnityPlugin
-    {
-        private Timer _timer;
+	[BepInPlugin("com.cactuspie.ramcleanerinterval", "CactusPie.RamCleanerInterval", "1.0.0")]
+	public class CustomRamCleanerIntervalPlugin : BaseUnityPlugin
+	{
+		private Timer timer;
 
-        private MethodInfo _emptyWorkingSetMethod;
+		public static ConfigEntry<string> CleanNow { get; set; }
 
-        public static ConfigEntry<string> CleanNow { get; set; }
+		internal static ConfigEntry<int> RamCleanerInterval { get; set; }
 
-        internal static ConfigEntry<int> RamCleanerInterval { get; set; }
+		internal static ConfigEntry<bool> IntervalEnabled { get; set; }
 
-        internal static ConfigEntry<bool> IntervalEnabled { get; set; }
+		internal static ConfigEntry<bool> OnlyInRaid { get; set; }
 
-        internal static ConfigEntry<bool> OnlyInRaid { get; set; }
+		[UsedImplicitly]
+		internal void Start()
+		{
+			const string sectionName = "Override RAM cleaner interval";
 
-        [UsedImplicitly]
-        internal void Start()
-        {
-            const string sectionName = "Override RAM cleaner interval";
+			IntervalEnabled = Config.Bind
+			(
+				sectionName,
+				"Interval enabled",
+				true,
+				new ConfigDescription
+				(
+					"Whether or not we should use the custom RAM cleaner interval",
+					null,
+					new ConfigurationManagerAttributes
+					{
+						Order = 4,
+					}
+				)
+			);
 
-            IntervalEnabled = Config.Bind
-            (
-                sectionName,
-                "Interval enabled",
-                true,
-                new ConfigDescription
-                (
-                    "Whether or not we should use the custom RAM cleaner interval",
-                    null,
-                    new ConfigurationManagerAttributes
-                    {
-                        Order = 4,
-                    }
-                )
-            );
+			RamCleanerInterval = Config.Bind
+			(
+				sectionName,
+				"Interval (seconds)",
+				300,
+				new ConfigDescription
+				(
+					"Number of seconds between each RAM cleaner execution. Changing this setting resets the interval",
+					new AcceptableValueRange<int>(30, 900),
+					new ConfigurationManagerAttributes
+					{
+						Order = 3,
+					}
+				)
+			);
 
-            RamCleanerInterval = Config.Bind
-            (
-                sectionName,
-                "Interval (seconds)",
-                300,
-                new ConfigDescription
-                (
-                    "Number of seconds between each RAM cleaner execution. Changing this setting resets the interval",
-                    new AcceptableValueRange<int>(30, 900),
-                    new ConfigurationManagerAttributes
-                    {
-                        Order = 3,
-                    }
-                )
-            );
+			CleanNow = Config.Bind(
+				sectionName,
+				"Clean now",
+				"Execute the RAM cleaner now",
+				new ConfigDescription(
+					"Execute the RAM cleaner now",
+					null,
+					new ConfigurationManagerAttributes
+					{
+						CustomDrawer = CleanNowButtonDrawer,
+						Order = 2,
+					}
+				));
 
-            CleanNow = Config.Bind(
-                sectionName,
-                "Clean now",
-                "Execute the RAM cleaner now",
-                new ConfigDescription(
-                    "Execute the RAM cleaner now",
-                    null,
-                    new ConfigurationManagerAttributes
-                    {
-                        CustomDrawer = CleanNowButtonDrawer,
-                        Order = 2,
-                    }
-                ));
+			OnlyInRaid = Config.Bind
+			(
+				sectionName,
+				"Only in raid",
+				true,
+				new ConfigDescription
+				(
+					"Only run the RAM cleaner in raid",
+					null,
+					new ConfigurationManagerAttributes
+					{
+						Order = 1,
+					}
+				)
+			);
 
-            OnlyInRaid = Config.Bind
-            (
-                sectionName,
-                "Only in raid",
-                true,
-                new ConfigDescription
-                (
-                    "Only run the RAM cleaner in raid",
-                    null,
-                    new ConfigurationManagerAttributes
-                    {
-                        Order = 1,
-                    }
-                )
-            );
+			timer = new Timer
+			{
+				AutoReset = true,
+				Interval = RamCleanerInterval.Value * 1000,
+			};
 
-            _timer = new Timer
-            {
-                AutoReset = true,
-                Interval = RamCleanerInterval.Value * 1000,
-            };
+			timer.Elapsed += TimerOnElapsed;
 
-            _timer.Elapsed += TimerOnElapsed;
+			RamCleanerInterval.SettingChanged += RamCleanerIntervalOnSettingChanged;
+			IntervalEnabled.SettingChanged += IntervalEnabledOnSettingChanged;
 
-            RamCleanerInterval.SettingChanged += RamCleanerIntervalOnSettingChanged;
-            IntervalEnabled.SettingChanged += IntervalEnabledOnSettingChanged;
+			if (IntervalEnabled.Value)
+			{
+				timer.Start();
+			}
+			else
+			{
+				timer.Stop();
+			}
+		}
 
-            _emptyWorkingSetMethod = (
-                from eftType in PatchConstants.EftTypes // Should be GClass693 as of 3.7.4
-                let emptyWorkingSetMethod = eftType.GetMethod("EmptyWorkingSet")
-                where emptyWorkingSetMethod != null && !emptyWorkingSetMethod.GetParameters().Any()
-                select emptyWorkingSetMethod
-            ).Single();
+		private void IntervalEnabledOnSettingChanged(object sender, EventArgs e)
+		{
+			if (IntervalEnabled.Value)
+			{
+				timer.Start();
+			}
+			else
+			{
+				timer.Stop();
+			}
+		}
 
-            if (IntervalEnabled.Value)
-            {
-                _timer.Start();
-            }
-            else
-            {
-                _timer.Stop();
-            }
-        }
+		private void RamCleanerIntervalOnSettingChanged(object sender, EventArgs e)
+		{
+			ChangeInterval(RamCleanerInterval.Value);
+		}
 
-        private void IntervalEnabledOnSettingChanged(object sender, EventArgs e)
-        {
-            if (IntervalEnabled.Value)
-            {
-                _timer.Start();
-            }
-            else
-            {
-                _timer.Stop();
-            }
-        }
+		private void TimerOnElapsed(object sender, ElapsedEventArgs e)
+		{
+			ExecuteCleaner();
+		}
 
-        private void RamCleanerIntervalOnSettingChanged(object sender, EventArgs e)
-        {
-            ChangeInterval(RamCleanerInterval.Value);
-        }
+		/// <summary>
+		/// Execute the ram cleaner
+		/// </summary>
+		/// <param name="force">If the ram cleaner should be executed even when not in raid</param>
+		private void ExecuteCleaner(bool force = false)
+		{
+			if (!force && OnlyInRaid.Value && !GameHelper.IsInGame())
+			{
+				return;
+			}
 
-        private void TimerOnElapsed(object sender, ElapsedEventArgs e)
-        {
-            ExecuteCleaner();
-        }
+			Logger.LogInfo("Executing the RAM cleaner");
 
-        /// <summary>
-        /// Execute the ram cleaner
-        /// </summary>
-        /// <param name="force">If the ram cleaner should be executed even when not in raid</param>
-        private void ExecuteCleaner(bool force = false)
-        {
-            if (!force && OnlyInRaid.Value && !GameHelper.IsInGame())
-            {
-                return;
-            }
+			GClass813.EmptyWorkingSet();
+		}
 
-            Logger.LogInfo("Executing the RAM cleaner");
+		private void CleanNowButtonDrawer(ConfigEntryBase entry)
+		{
+			bool button = GUILayout.Button("Clean now", GUILayout.ExpandWidth(true));
+			if (button)
+			{
+				ExecuteCleaner(true);
+			}
+		}
 
-            _emptyWorkingSetMethod.Invoke(null, null);
-        }
-
-        private void CleanNowButtonDrawer(ConfigEntryBase entry)
-        {
-            bool button = GUILayout.Button("Clean now", GUILayout.ExpandWidth(true));
-            if (button)
-            {
-                ExecuteCleaner(true);
-            }
-        }
-
-        private void ChangeInterval(int interval)
-        {
-            if (_timer.Enabled)
-            {
-                _timer.Stop();
-                _timer.Interval = interval * 1000;
-                _timer.Start();
-            }
-            else
-            {
-                _timer.Interval = interval * 1000;
-            }
-        }
-    }
+		private void ChangeInterval(int interval)
+		{
+			if (timer.Enabled)
+			{
+				timer.Stop();
+				timer.Interval = interval * 1000;
+				timer.Start();
+			}
+			else
+			{
+				timer.Interval = interval * 1000;
+			}
+		}
+	}
 }
